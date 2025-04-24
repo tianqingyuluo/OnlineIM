d51018ea4f953d869879ceacbf7e9c8fa86975fb
tianqingyuluo <chengwanhao2017@163.com>
2025-04-24 16:24:05 +0800

    Move project files to 'onlineIM-server' directory
    Reorganized project structure by moving all files from the root to the newly created 'onlineIM-server' directory. This improves clarity and lays the groundwork for potential multi-module project expansion.

---
+++ onlineIM-server/src/main/java/icu/tianqingyuluo/onlineim/pojo/entity/User.java
@@ -0,0 +1,13 @@
+package icu.tianqingyuluo.onlineim.pojo.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String userid;
+    private String username;
+    private String nickname;
+    private String password;
+    private String avatarurl;
+    private String createat;
+}
