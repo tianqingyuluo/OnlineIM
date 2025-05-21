3f44eac7bd31c889697dd050c32d780343778bc2
ymx545 <3471830049@qq.com>
2025-05-21 19:57:39 +0800

    feat:完成了GroupAnnouncementService

--- onlineIM-server/src/main/java/icu/tianqingyuluo/onlineim/service/GroupAnnouncementService.java
+++ onlineIM-server/src/main/java/icu/tianqingyuluo/onlineim/service/GroupAnnouncementService.java
@@ -1,8 +1,23 @@
 package icu.tianqingyuluo.onlineim.service;
 
+import icu.tianqingyuluo.onlineim.pojo.dto.request.GroupAnnouncementRequest;
+import icu.tianqingyuluo.onlineim.pojo.dto.response.GroupAnnouncementResponse;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface GroupAnnouncementService {
 
+    List<GroupAnnouncementResponse> getAnnouncementsByGroupId(String groupId);
+
+    GroupAnnouncementResponse getAnnouncementById(String groupId, String announcementId);
+
+    GroupAnnouncementResponse createAnnouncement(String groupId, GroupAnnouncementRequest request, String publisherId);
+
+    GroupAnnouncementResponse updateAnnouncement(String groupId, String announcementId, GroupAnnouncementRequest request, String updaterId);
+
+    void deleteAnnouncement(String groupId, String announcementId, String deleterId);
+
+    void pinAnnouncement(String groupId, String announcementId, Boolean pinStatus, String operatorId);
 }
